625987566dc8ff521b44e4b32b264582f6474486
Rintu-chowdory <chowdoryrintu492@gmail.com>
2026-03-31 19:32:11 +0200

    Create About component

---
+++ src/components/About.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className="about">
+            <h1>About Me</h1>
+            <p>
+                Welcome to my portfolio! I am a passionate developer focused on delivering effective
+                solutions through automation and best practices in CI/CD.
+            </p>
+            <p>
+                My expertise includes:
+            </p>
+            <ul>
+                <li>Continuous Integration</li>
+                <li>Continuous Deployment</li>
+                <li>Infrastructure as Code</li>
+                <li>Cloud Services</li>
+                <li>Containerization (Docker, Kubernetes)</li>
+            </ul>
+            <p>
+                Thank you for visiting!
+            </p>
+        </div>
+    );
+};
+
+export default About;
